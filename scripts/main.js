a320428d574e1d3dcc80217b3a46b0284d203e37
Jane Jeong <janejeong@kakao.com>
2020-11-27 19:36:10 +0900

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -13,35 +13,40 @@ var x = setInterval(function() {
     let minutes = today.getMinutes();  // 분
     let seconds = today.getSeconds();  // 초
     // let milliseconds = today.getMilliseconds(); // 밀리초
-    document.getElementById("today").innerHTML = 'Time now { here } : ' + year + ' ' + month + ' ' + date + '   ' + hours + ':' + minutes + ':' + seconds 
+    document.getElementById("today").innerHTML = 'Time now : ' + year + ' ' + month + ' ' + date + '   ' + hours + ':' + minutes + ':' + seconds 
 }, 1000);
+
+
+
+
+
 
 /* Countup Timer */
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 8, 1993 09:05:00").getTime();
+let countDownDate = new Date("Jun 8, 1993 09:05:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+let xx = setInterval(function() {
 
   // Get today's date and time
-  var now = new Date().getTime();
+  let now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance = now - countDownDate;
+  let distance = now - countDownDate;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("counttime").innerHTML = "Since { my } birth : " + days + "d " + hours + "h "
+  document.getElementById("timer").innerHTML = "Since my birth : " + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("counttime").innerHTML = "EXPIRED";
+    clearInterval(xx);
+    document.getElementById("timer").innerHTML = "EXPIRED";
   }
 }, 1000);
